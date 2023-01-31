8814ed6516bb67cfa684faba6154d1c63364f9ad
Lemoine Charlotte <lemoine.charlotte@gmail.com>
2023-01-31 13:40:02 +0100

    change error sameSite

--- js/js-cookie.js
+++ js/js-cookie.js
@@ -1,18 +1,21 @@
 var cookieModal = document.getElementById("cookie-modal");
 var acceptBtn = document.getElementById("accept-cookie");
-// Check if the cookie has already been accepted
-if (Cookies.get("cookie-consent")) {
-  cookieModal.style.display = "none";
-}
-else {
-  acceptBtn.onclick = function () {
-    if ("cookie-consent" == "accepted") {
-      cookieModal.style.display = 'none';
-    }
+
+// cookieModal.style.display = "block";
+
+// acceptBtn.onclick = function () {
+//   cookieModal.style.display = "none";
+//   Cookies.set("cookie-consent", "accepted", { expires: 365, sameSite: "none" ,secure:true});};
+ 
+
+  if (!Cookies.get("cookie-consent")) {
+    acceptBtn.onclick = function () {
+      cookieModal.style.display = "none";
+      Cookies.set("cookie-consent", "accepted", { expires: 365, sameSite: "none", secure: true });
+    };
+  }
+  else {
+    
     cookieModal.style.display = "none";
-    Cookies.set("cookie-consent", "accepted", { expires: 365, sameSite: "none", secure: true });
-  };
-}
-
-
-
+  }
+  
