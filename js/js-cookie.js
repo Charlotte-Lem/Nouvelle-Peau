310620a34f94c8a23d128cd67f09b8f93808c880
Lemoine Charlotte <lemoine.charlotte@gmail.com>
2023-02-24 15:46:47 +0100

    change js cookie

--- js/js-cookie.js
+++ js/js-cookie.js
@@ -1,21 +1,14 @@
+
 var cookieModal = document.getElementById("cookie-modal");
 var acceptBtn = document.getElementById("accept-cookie");
 
-// cookieModal.style.display = "block";
-
-// acceptBtn.onclick = function () {
-//   cookieModal.style.display = "none";
-//   Cookies.set("cookie-consent", "accepted", { expires: 365, sameSite: "none" ,secure:true});};
- 
-
-  if (!Cookies.get("cookie-consent")) {
-    acceptBtn.onclick = function () {
-      cookieModal.style.display = "none";
-      Cookies.set("cookie-consent", "accepted", { expires: 365, sameSite: "none", secure: true });
-    };
-  }
-  else {
-    
+if (!Cookies.get("cookie-consent")) {
+  acceptBtn.onclick = function () {
     cookieModal.style.display = "none";
-  }
-  
+    Cookies.set("cookie-consent", "accepted", { expires: 365, sameSite: "None" });
+    location.reload();
+  };
+}
+else {
+  cookieModal.style.display = "none";
+}
